f8879412f38ca8b3e3d57defdd098c237ed488d8
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-06-24 17:56:47 +0100

    Add animation to 'Coming soon..'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
+import ReactAnime from 'react-animejs'
 
-function App() {
+const {Anime, stagger} = ReactAnime
+
+const App = () => {
+  const textToAnimate = 'Coming soon...'
+  // const timeline = anime.timeline({ targets: '.visualiser', delay: anime.stagger(100), translateY: 50 })
+  //   .add({ delay: anime.stagger(100), translateY: -50 })
+
+  // const visualiser = document.querySelector('visualiser')
+  // timeline.play()
+
   return (
     <div className="App">
-      <div>
-        Coming soon...
-      </div>
+      <Anime className='visualiser' initial={[
+        {
+          targets: ".character",
+          keyframes: [
+            { translateY: -80 },
+            { translateY: 0 }
+          ],
+          easing: 'cubicBezier(.5, .05, .1, .3)',
+          loop: true,
+          delay: stagger(100)
+        }
+      ]}>
+        {textToAnimate.split('').map(character => {
+          return <div className='character'>{character}</div>
+        })}
+      </Anime>
+
     </div>
   );
 }
