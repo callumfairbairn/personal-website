e45dc40a8f1302da7a6caf7764cbe517850d47f1
callumfairbairn <callum.fairbairn@thoughtworks.com>
2020-08-06 16:02:18 +0100

    Star animation

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Star.scss'
-import { motion } from 'framer-motion'
+import { motion, useAnimation } from 'framer-motion'
+
+const initial = {
+  rotate: [0, 180, 360], scale: [1, 1.5, 1],
+  transition: {
+    duration: 2, times: [0, 0.5, 1], loop: Infinity
+  }
+}
+
+const hoveredStart = {
+  rotate: [null, 360], scale: [null, 40],
+  zIndex: 1
+}
+
+const hoveredEnd = {
+  rotate: [null, 0], scale: [null, 1],
+  zIndex: 0
+}
 
 export const Star = ({ content }) => {
+  // const [isHovered, setIsHovered] = useState(false)
+  const controls = useAnimation()
+  controls.start(initial)
+
   return content
-    ? <motion.div animate={{ rotate: [0, 180, 360], scale: [1, 1.5, 1] }} transition={{ duration: 2, times: [0, 0.5, 1], loop: Infinity }} className='star-with-content' />
+    ? <motion.div
+      animate={controls}
+      // onHoverStart={() => { setIsHovered(true) }}
+      // onHoverEnd={() => { setIsHovered(false) }}
+      onHoverStart={() => { controls.start(hoveredStart) }}
+      onHoverEnd={async () => {
+        await controls.start(hoveredEnd)
+        await controls.start(initial)
+      }}
+      className='star-with-content'
+    />
     : <div className='star' />
 }
